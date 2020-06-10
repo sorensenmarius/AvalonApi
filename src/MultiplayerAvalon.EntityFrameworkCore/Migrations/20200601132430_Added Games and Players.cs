e8736a19349af1e9b2c21afce813874789a0952f
Marius Sørensen <jobb@s0rensen.no>
2020-06-10 20:37:23 +0200

    Migrations?

---
+++ src/MultiplayerAvalon.EntityFrameworkCore/Migrations/20200601132430_Added Games and Players.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MultiplayerAvalon.Migrations
+{
+    public partial class AddedGamesandPlayers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AbpOrganizationUnits_TenantId_Code",
+                table: "AbpOrganizationUnits");
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    JoinCode = table.Column<int>(nullable: false),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 64, nullable: false),
+                    Role = table.Column<int>(nullable: false),
+                    GameId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Players_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpOrganizationUnits_TenantId_Code",
+                table: "AbpOrganizationUnits",
+                columns: new[] { "TenantId", "Code" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_GameId",
+                table: "Players",
+                column: "GameId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AbpOrganizationUnits_TenantId_Code",
+                table: "AbpOrganizationUnits");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpOrganizationUnits_TenantId_Code",
+                table: "AbpOrganizationUnits",
+                columns: new[] { "TenantId", "Code" },
+                unique: true,
+                filter: "[TenantId] IS NOT NULL");
+        }
+    }
+}
