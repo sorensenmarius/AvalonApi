e0d256a0fb73249a437e16a8a32ced4cde1e8e3d
Marius Sørensen <jobb@s0rensen.no>
2020-09-10 08:38:59 +0200

    Fixed concurrency issue with voting

---
+++ src/MultiplayerAvalon.Core/AppDomain/Rounds/IRoundRepository.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplayerAvalon.AppDomain.Rounds
+{
+    public interface IRoundRepository: IRepository<Round, Guid>
+    {
+        public Task<int> VoteForTeam(Guid roundId);
+        public Task<int> VoteAgainstTeam(Guid roundId);
+        public Task<int> VoteForExpedition(Guid roundId);
+        public Task<int> VoteAgainstExpedition(Guid roundId);
+    }
+}
---
+++ src/MultiplayerAvalon.EntityFrameworkCore/Migrations/20200909201328_Add-Stored-Procedures.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MultiplayerAvalon.Migrations
+{
+    public partial class AddStoredProcedures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var procs = @"
+							GO
+								CREATE OR ALTER PROCEDURE [dbo].[VoteForTeam]
+									(
+										@RoundId uniqueidentifier = NULL,
+										@TotalVotes int output
+									)
+									AS
+										UPDATE Rounds SET VotesForTeam = VotesForTeam + 1 WHERE Id = @RoundId
+										SET @TotalVotes = (SELECT VotesForTeam + VotesAgainstTeam AS TotalVotes FROM Rounds WHERE Id = @RoundId)
+
+							GO
+								CREATE OR ALTER PROCEDURE [dbo].[VoteAgainstTeam]
+									(
+										@RoundId uniqueidentifier = NULL,
+										@TotalVotes int output
+									)
+									AS
+										UPDATE Rounds SET VotesAgainstTeam = VotesAgainstTeam + 1 WHERE Id = @RoundId
+										SET @TotalVotes = (SELECT VotesForTeam + VotesAgainstTeam AS TotalVotes FROM Rounds WHERE Id = @RoundId)
+	
+							GO
+								CREATE OR ALTER PROCEDURE [dbo].[VoteForExpedition]
+									(
+										@RoundId uniqueidentifier = NULL,
+										@TotalVotes int output
+									)
+									AS
+										UPDATE Rounds SET MissionVoteGood = MissionVoteGood + 1 WHERE Id = @RoundId
+										SET @TotalVotes = (SELECT MissionVoteBad + MissionVoteGood AS TotalVotes FROM Rounds WHERE Id = @RoundId)
+
+							GO
+								CREATE OR ALTER PROCEDURE [dbo].[VoteAgainstExpedition]
+									(
+										@RoundId uniqueidentifier = NULL,
+										@TotalVotes int output
+									)
+									AS
+										UPDATE Rounds SET MissionVoteBad = MissionVoteBad + 1 WHERE Id = @RoundId
+										SET @TotalVotes = (SELECT MissionVoteBad + MissionVoteGood AS TotalVotes FROM Rounds WHERE Id = @RoundId)
+			
+
+
+			
+                        ";
+            migrationBuilder.Sql(procs);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
