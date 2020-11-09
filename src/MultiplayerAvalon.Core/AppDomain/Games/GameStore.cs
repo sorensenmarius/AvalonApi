6c9875f32eb2aefaf3c8d075ed5eb0270dc03b98
Marius Sørensen <jobb@s0rensen.no>
2020-11-09 20:37:07 +0100

    Added singleton

---
+++ src/MultiplayerAvalon.Core/AppDomain/Games/GameStore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiplayerAvalon.AppDomain.Games
+{
+    public sealed class GameStore
+    {
+        private static readonly GameStore instance = new GameStore();
+        public Dictionary<Guid, Game> Games;
+        static GameStore()
+        {
+        }
+
+        private GameStore()
+        {
+            Games = new Dictionary<Guid, Game>();
+        }
+
+        public static GameStore Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public static void AddOrUpdateGame(Game g)
+        {
+            instance.Games[g.Id] = g;
+        }
+
+        public static void DeleteGame(Guid id)
+        {
+            instance.Games.Remove(id);
+        }
+    }
+}
